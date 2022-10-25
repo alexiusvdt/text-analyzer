0f8a6e22c6af3304912f5a99a49b7d06218a43c0
Alex Johnson <alex.johnson293@gmail.com>
2022-10-25 14:14:06 -0700

    complete text-analyzer lesson
    Co-authored-by: Ruben Giosa <rubenalexgiosa@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
+// Utility Logic
+function isEmpty(testString) {
+  return (testString.trim().length === 0);
+}
+
 // Business Logic
 
 function wordCounter(text) {
-  if (text.trim().length === 0) {
+  if (isEmpty(text)) {
     return 0;
   }
   let wordCount = 0;
@@ -15,6 +20,9 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
+  if (isEmpty(word) || isEmpty(text)) {
+    return 0;
+  }
   const textArray = text.split(" ");
   let wordCount = 0;
   textArray.forEach(function(element) {
@@ -23,6 +31,27 @@ function numberOfOccurrencesInText(word, text) {
     }
   });
   return wordCount;
+}
+
+function boldPassage(word, text) {
+  if (isEmpty(word) || isEmpty(text)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      p.append(" ");
+    }
+  });
+  return p;
 }
 
 // UI Logic
@@ -34,11 +63,19 @@ function handleFormSubmission() {
   const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
   document.getElementById("total-count").innerText = wordCount;
   document.getElementById("selected-count").innerText = occurrencesOfWord;
+  // new lines here!
+  let boldedPassage = boldPassage(word, passage);
+  if (boldedPassage) {
+    document.querySelector("div#bolded-passage").append(boldedPassage);
+  } else {
+    document.querySelector("div#bolded-passage").innerText = null;
+  }
 }
 
 window.addEventListener("load", function() {
   document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
 });
+
 
 
 //old practice code, reference only do not uncomment
@@ -87,7 +124,6 @@ window.addEventListener("load", function() {
 //       textArray.pop(word)
 //   })
 //   return cleanArray
-}
 
 // our version
 // function numberOfOccurencesInText(word, text) {
